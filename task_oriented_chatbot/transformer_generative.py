1fc792dacd0182f939a99b8c6cfc950a610b8ce7
shashank.maiya <shashank.maiya@servicenow.com>
2020-10-23 00:29:48 -0700

    Using evalModel for predict

--- task_oriented_chatbot/transformer_generative.py
+++ task_oriented_chatbot/transformer_generative.py
@@ -11,6 +11,8 @@ from parlai.scripts.interactive import Interactive
 from parlai.scripts.display_data import DisplayData
 from parlai.scripts.train_model import TrainModel
 from parlai.scripts.display_model import DisplayModel
+from parlai.scripts.eval_model import EvalModel
+
 
 pretrained_baseline = {
 'baseline_model' : 'transformer/generator', # Baseline model architecture used
@@ -20,41 +22,33 @@ pretrained_baseline = {
 'predict_dataset' : 'customer_care', # Dataset used for predicting (test.txt)
 'predict_model_file' : 'zoo:tutorial_transformer_generator/model' # Path of the model which will be used for prediction
 }
-
-# Reusing the fine tuned models for ED. They seem to be in a different location. Download them into finetuned_ed/model and call predict_main
-# wget https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/models/normal_transformer_pretrained.mdl  # Normal Transformer, pretrained
-# wget https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/models/normal_transformer_finetuned.mdl  # Normal Transformer, fine-tuned
-# wget https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/models/bert_pretrained.mdl  # BERT, pretrained
-# wget https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/models/bert_finetuned.mdl  # BERT, fine-tuned
-# wget https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/models/bert_finetuned_emoprepend1.mdl  # BERT, fine-tuned (EmoPrepend-1)
-# wget https://dl.fbaipublicfiles.com/parlai/empatheticdialogues/models/fasttext_empathetic_dialogues.mdl  # fastText classifier used for EmoPrepend-1
 finetuned_ed = {
-'baseline_model' : 'transformer/generator',
-'init_model' : 'zoo:tutorial_transformer_generator/model',
-'fine_tune_dataset' : 'empathetic_dialogues',
-'train_model_file' : 'finetuned_ed/model',
-'predict_dataset' : 'customer_care',
-'predict_model_file' : 'zoo:/model'
+'baseline_model' : 'transformer/generator', # Baseline model architecture used
+'init_model' : 'zoo:tutorial_transformer_generator/model', # The initial model to use for fine-tuning
+'fine_tune_dataset' : 'empathetic_dialogues', # Dataset used for fine tuning (train.txt and valid.txt)
+'train_model_file' : 'finetuned_ed/model', # Path where the trained model is saved
+'predict_dataset' : 'customer_care', # Dataset used for predicting (test.txt)
+'predict_model_file' : 'finetuned_ed/model' # Path of the model which will be used for prediction
 }
 finetuned_cc = {
-'baseline_model' : 'transformer/generator',
-'init_model' : 'zoo:tutorial_transformer_generator/model',
-'fine_tune_dataset' : 'customer_care',
-'train_model_file' : 'finetuned_cc/model',
-'predict_dataset' : 'customer_care',
-'predict_model_file' : 'finetuned_cc/model'
+'baseline_model' : 'transformer/generator', # Baseline model architecture used
+'init_model' : 'zoo:tutorial_transformer_generator/model', # The initial model to use for fine-tuning
+'fine_tune_dataset' : 'customer_care', # Dataset used for fine tuning (train.txt and valid.txt)
+'train_model_file' : 'finetuned_cc/model', # Path where the trained model is saved
+'predict_dataset' : 'customer_care', # Dataset used for predicting (test.txt)
+'predict_model_file' : 'finetuned_cc/model' # Path of the model which will be used for prediction
 }
 finetuned_ed_cc = {
-'baseline_model' : 'transformer/generator',
-'init_model' : 'finetuned_ed/model',
-'fine_tune_dataset' : 'customer_care',
-'train_model_file' : 'finetuned_ed_cc/model',
-'predict_dataset' : 'customer_care',
-'predict_model_file' : 'finetuned_ed_cc/model'
+'baseline_model' : 'transformer/generator', # Baseline model architecture used
+'init_model' : 'finetuned_ed/model', # The initial model to use for fine-tuning
+'fine_tune_dataset' : 'customer_care', # Dataset used for fine tuning (train.txt and valid.txt)
+'train_model_file' : 'finetuned_ed_cc/model', # Path where the trained model is saved
+'predict_dataset' : 'customer_care', # Dataset used for predicting (test.txt)
+'predict_model_file' : 'finetuned_ed_cc/model' # Path of the model which will be used for prediction
 }
 
-models_to_train = [finetuned_cc, finetuned_ed_cc]
-models_to_test = [pretrained_baseline, finetuned_ed, finetuned_cc, finetuned_ed_cc]
+models_to_train = [finetuned_ed, finetuned_cc, finetuned_ed_cc]
+models_to_predict = [pretrained_baseline, finetuned_ed, finetuned_cc, finetuned_ed_cc]
 
 def display_main(dataset):
     DisplayData.main(task=dataset, num_examples=5, datatype='train')
@@ -69,8 +63,7 @@ def train_main(model_dict):
     model_file=model_dict['train_model_file'],
     
     # initialize with a pretrained model
-    if model_dict['init_model']:
-        init_model=model_dict['init_model'],
+    init_model=model_dict['init_model'],
     
     # arguments we get from the pretrained model.
     # Unfortunately, these must be looked up separately for each model.
@@ -88,7 +81,7 @@ def train_main(model_dict):
     # early stopping on perplexity
     validation_metric='ppl',
     # train at most 10 minutes, and validate every 0.25 epochs
-    max_train_time=10*60, validation_every_n_epochs=0.01,
+    max_train_time=2*60, validation_every_n_epochs=0.01,
     
     # depend on your gpu. If you have a V100, this is good
     batchsize=12, fp16=True, fp16_impl='mem_efficient',
@@ -100,13 +93,22 @@ def train_main(model_dict):
     dynamic_batching='full',
     )
 
-def predict_main(model_dict):
-    DisplayModel.main(
-    task=model_dict['predict_dataset'],
-    model_file=model_dict['predict_model_file'],
-    num_examples=2,
-    skip_generation=False
-    )
+def predict_main(model_dict, display=False):
+    if display:
+        DisplayModel.main(
+        task=model_dict['predict_dataset'],
+        model_file=model_dict['predict_model_file'],
+        num_examples=2,
+        datatype='test'
+        )
+    else:
+        EvalModel.main(
+        task=model_dict['predict_dataset'],
+        model_file=model_dict['predict_model_file'],
+        metrics =  ['ppl','f1','accuracy','hits@1'],
+        num_examples=5,
+        datatype='test'
+        )
 
 
 if __name__ == "__main__":
@@ -115,13 +117,6 @@ if __name__ == "__main__":
         train_main(model)
     for model in models_to_predict:
         predict_main(model)
-
-
-
-
-
-
-
 
 
 
